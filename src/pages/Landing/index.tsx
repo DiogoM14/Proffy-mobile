d80f42b73b50b6e1b6959e6254e001e2e7f8ca93
Xavi002 <diogomartins200214@gmail.com>
2020-08-11 10:15:51 +0100

    first commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View,  Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Landing: React.FC = () => {
+  return (
+    <Container>
+      <Text>Hi</Text>
+    </Container>
+  );
+}
+
+export default Landing;
