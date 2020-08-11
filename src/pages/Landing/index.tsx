ed2c5a0920142920df8d181c01639931b024f594
Xavi002 <diogomartins200214@gmail.com>
2020-08-11 11:40:53 +0100

    finished landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container, Image, Title, TitleBold } from './styles';
+import { Container, LandingImage, Title, TitleBold, ButtonsContainer, Button, ButtonText, TotalConnections } from './styles';
 
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <Image source={landingImg} style={{resizeMode: 'contain'}} />
+      <LandingImage source={landingImg} style={{resizeMode: 'contain'}} />
 
       <Title>
         Bem vindo, {'\n'}
         <TitleBold>O que desejas fazer?</TitleBold>
       </Title>
+
+      <ButtonsContainer>
+        <Button style={{backgroundColor: '#9871f5'}}>
+          <Image source={studyIcon} />
+
+          <ButtonText>Estudar</ButtonText>
+        </Button>
+
+        <Button style={{backgroundColor: '#04d361'}}>
+          <Image source={giveClassesIcon} />
+
+          <ButtonText>Dar aulas</ButtonText>
+        </Button>
+      </ButtonsContainer>
+
+      <TotalConnections>
+        Total de 230 conceções já realizadas {' '}
+        <Image source={heartIcon} />
+      </TotalConnections>
     </Container>
   );
 }
--- src/pages/Landing/styles.tsx
+++ src/pages/Landing/styles.tsx
@@ -7,7 +7,7 @@ export const Container = styled.View`
   padding: 40px;
 `;
 
-export const Image = styled.Image`
+export const LandingImage = styled.Image`
   width: 100%;
 `;
 
@@ -23,4 +23,32 @@ export const TitleBold = styled.Text`
   font-family: Poppins_600SemiBold;
 `;
 
+export const ButtonsContainer = styled.View`
+  flex-direction: row;
+  margin-top: 40px;
+  justify-content: space-between;
+`;
 
+export const Button = styled.TouchableOpacity`
+  height: 150px;
+  width: 48%;
+  background: #333;
+  border-radius: 8px;
+  padding: 24px;
+  justify-content: space-between;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'Archivo_700Bold';
+  color: #fff;
+  font-size: 18px;
+`;
+
+export const TotalConnections = styled.Text`
+  font-family: Poppins_400Regular;
+  color: #d4c2ff;
+  font-size: 12px;
+  line-height: 20px;
+  max-width: 150px;
+  margin-top: 40px;
+`;
