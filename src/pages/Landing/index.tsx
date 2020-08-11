535e86429d2f35e04ae701c71171d95473dc7203
Xavi002 <diogomartins200214@gmail.com>
2020-08-11 10:39:50 +0100

    importing fonts

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View,  Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Image, Title, TitleBold } from './styles';
+
+import landingImg from '../../assets/images/landing.png';
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <Text>Hi</Text>
+      <Image source={landingImg} style={{resizeMode: 'contain'}} />
+
+      <Title>
+        Bem vindo, {'\n'}
+        <TitleBold>O que desejas fazer?</TitleBold>
+      </Title>
     </Container>
   );
 }
--- src/pages/Landing/styles.tsx
+++ src/pages/Landing/styles.tsx
@@ -1,5 +1,26 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  
+  flex: 1;
+  background-color: #8257e5;
+  justify-content: center;
+  padding: 40px;
 `;
+
+export const Image = styled.Image`
+  width: 100%;
+`;
+
+export const Title = styled.Text`
+  font-family: Archivo_400Regular;
+  color: #fff;
+  font-size: 20px;
+  line-height: 30px;
+  margin-top: 80px;
+`;
+
+export const TitleBold = styled.Text`
+  font-family: Poppins_600SemiBold;
+`;
+
+
