f70c662f7b64f241ee1989b6dc8f7060a2965ce7
Xavi002 <diogomartins200214@gmail.com>
2020-08-14 12:12:13 +0100

    finished beta

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -7,6 +7,7 @@ import { Container, Profile, Avatar, ProfileInfo, Name, Subject, Bio, Footer, Pr
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -27,6 +28,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
 
   function handleLinkToWhatsapp() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
   }
 
